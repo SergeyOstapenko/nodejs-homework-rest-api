9df76819de18763f405038efc5860dabbf0e4e9c
SergeyOstapenko <sergiy.v.ostapenko@gmail.com>
2022-10-03 21:52:58 +0300

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,61 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
 
-const listContacts = async () => {}
+const path = require("path");
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(contacts);
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
 
-const updateContact = async (contactId, body) => {}
+  const filteredContact = contacts.find(({ id }) => id === contactId);
+
+  return filteredContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((el) => el.id === String(contactId));
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return result;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+
+  const ids = contacts.map((item) => +item.id);
+  const contact = { id: String(Math.max(...ids) + 1), name, email, phone };
+
+  contacts.push(contact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contact;
+};
+
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+
+  const contact = await getContactById(contactId);
+  const updetedContact = { ...contact, ...body };
+
+  const index = contacts.findIndex((el) => el.id === String(contactId));
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1, updetedContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return updetedContact;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +63,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
